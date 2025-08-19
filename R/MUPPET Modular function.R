0cea9e892b1698636343ed1263d97ba3f876404a
Roy-Levy <Roy.Levy@asu.edu>
2025-08-19 13:43:15 -0700

    Update package

--- R/MUPPET Modular function.R
+++ R/MUPPET Modular function.R
@@ -644,14 +644,14 @@ MUPPET.modular.function <- function(
                 print("Checkpoint 7")
                 sink()
 
-                # # Write out the table
-                # if(nrow(table.to.write) >0){
-                #   file.name <- paste0(fragments[[which.fragment]]$name, " unstandardized loadings summary statistics.docx")
-                #   # tt(table.to.write) |> save_tt(paste0(file.name), overwrite=TRUE)
-                #   temp.table.tt <- tinytable::tt(table.to.write)
-                #   tinytable::save_tt(temp.table.tt, paste0(file.name), overwrite=TRUE)
-                #
-                # }
+                # Write out the table
+                if(nrow(table.to.write) >0){
+                  file.name <- paste0(fragments[[which.fragment]]$name, " unstandardized loadings summary statistics.docx")
+                  tt(table.to.write) |> save_tt(paste0(file.name), overwrite=TRUE)
+                  # temp.table.tt <- tinytable::tt(table.to.write)
+                  # tinytable::save_tt(temp.table.tt, paste0(file.name), overwrite=TRUE)
+
+                }
 
                 sink("Checkpoint.out", append = TRUE)
                 print("Checkpoint 8")
